23629d10285a33ceee921b107e01506bd4fb6725
Roy Sprowl <v-royspr@microsoft.com>
2018-02-12 15:20:42 -0800

    first sample code

---
+++ build_all/ios/samples/AMQPWS-Client-Sample/AMQPWS Client Sample-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef azureiot_Bridging_Header_h
+#define azureiot_Bridging_Header_h
+
+#import "iothub_client.h"
+#import "iothub_client_options.h"
+#import "iothub_message.h"
+#import "azure_c_shared_utility/threadapi.h"
+#import "azure_c_shared_utility/crt_abstractions.h"
+#import "azure_c_shared_utility/platform.h"
+#import "iothubtransportamqp_websockets.h"
+//#import "iothubtransportmqtt_websockets.h"
+//#import "iothubtransportmqtt.h"
+#import "iothubtransporthttp.h"
+
+#endif /* azureiot_Bridging_Header_h */
